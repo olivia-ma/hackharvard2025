c6ee8a96de89d1e57098b257c976ebc3cc0eefdd
Rshres <riwaz@bu.edu>
2025-10-05 01:54:30 -0400

    moved all the games to local

--- src/pages/FlappyBird.jsx
+++ src/pages/FlappyBird.jsx
@@ -3,9 +3,9 @@ import GameTemplate from "./GameTemplate";
 
 export default function FlappyBird() {
   return (
-    <GameTemplate
-      title="Flappy Bird"
-      iframeSrc="https://r1w125.github.io/JS-Flappy-Bird/"
-    />
+    <div>
+      <h2>FlappyBird</h2>
+      <iframe src="/flappy/index.html" width="100%" height="600" style={{ border: "none" }} title="Flappy Game" />
+    </div>
   );
 }
