b698745a966efd157f6601980d6d816174386c96
Rshres <riwaz@bu.edu>
2025-10-05 02:34:39 -0400

    removing scrollbars

--- src/pages/FlappyBird.jsx
+++ src/pages/FlappyBird.jsx
@@ -3,9 +3,10 @@ import GameTemplate from "./GameTemplate";
 
 export default function FlappyBird() {
   return (
-    <div>
-      <h2>FlappyBird</h2>
-      <iframe src="/flappy/index.html" width="100%" height="600" style={{ border: "none" }} title="Flappy Game" />
-    </div>
+    <GameTemplate 
+      title="Flappy Bird" 
+      iframeSrc="/flappy/index.html"
+      iframeHeight={800}
+    />
   );
 }
